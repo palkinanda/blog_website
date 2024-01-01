790fa1a058d0ebd2f5af3e09d28dfc10a0830006
Palki Nanda <you@example.com>
2024-01-01 23:02:11 +0530

    commit2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-[#ffffff] py-10 ">
-          <span className=" mt-2 align-middle self-center  text-white ml-4 font-semibold cursor-pointer">Build and Maintained by <span className='text-[#ff4545]'>Nikhil Jha </span>- nikhilaryan@gmail.com and <span className='text-[#ff4545]'>Palki Nanda</span> - palkinaanda@gmail.com</span>
+          <span className=" mt-2 align-middle self-center  text-white ml-4 font-semibold cursor-pointer">Maintained and Developed by Nikhil Jha - nikhilaryan2016@gmail.com and Palki Nnada - palkinaanda@gmail.com</span>
       </div>
     </div>
   );
